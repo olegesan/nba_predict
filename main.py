73ca3ad76ab9348be7da6228125e64b2de23c14e
Oleg <obazylnikov@ggc.edu>
2021-03-27 23:08:01 -0400

    initial commit with all games from 12-13 season

---
+++ main.py
@@ -0,0 +1,32 @@
+from nba_api.stats.endpoints  import leaguegamelog, commonplayerinfo, boxscoretraditionalv2
+import requests
+from os import mkdir
+import json
+years = [13]
+games_info = {}
+for year in years:
+    try:
+        season = leaguegamelog.LeagueGameLog(season=f"20{year}-{year+1}")
+        try:
+            games = season.get_normalized_dict()['LeagueGameLog']
+            game_ids = set([game['GAME_ID'] for game in games])
+            for game_id in game_ids:
+                games_info[game_id] = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=game_id).get_normalized_dict()
+                print(game_id)
+        except requests.exceptions.Timeout:
+            print("timeout inside second except")
+    except requests.exceptions.Timeout:
+        print("timeout insdie first excepty")
+
+f = open("./all_games12-13", "w")
+json.dump(games_info,f)
+f.close()
+# f.write(season_19_20_games.get_normalized_json())
+# f.close()
+# print(season_19_20_games.get_json())
+
+
+# player_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
+# print(player_info.get_json())
+# res = requests.request(url="https://google.com", method="GET");
+# print(res)
